0c32ff6a1d994834e9534c4a9acaaff12f06a8af
DataMind <rune@battlekart.com>
2025-09-16 10:30:53 +0200

    Development commit

--- estate/models/estate_estate_property.py
+++ estate/models/estate_estate_property.py
@@ -4,6 +4,7 @@ from odoo import fields, models
 # Create the new model
 class EstateProperty(models.Model):
     # Define model metadata
-    _name = 'estate.property' # Table name
+    _name = 'estate.property'
+    _description = 'Estate Property'
 
     # Define fields
