3b117375b9dabbbe3caf5799c37af8bfbe632319
DataMind <rune@battlekart.com>
2025-09-16 10:28:07 +0200

    Development commit

--- estate/models/estate_estate_property.py
+++ estate/models/estate_estate_property.py
@@ -1,16 +1,9 @@
+# Imports
 from odoo import fields, models
 
-# Create a new model
+# Create the new model
 class EstateProperty(models.Model):
     # Define model metadata
-    _name = 'estate.property'
-    _description = 'Estate Property'
+    _name = 'estate.property' # Table name
 
     # Define fields
-    name = fields.Char(required=True)
-    description = fields.Text()
-    date_availability = fields.Date()
-    expected_price = fields.Float()
-    bedrooms = fields.Integer()
-    garden = fields.Boolean()
-    garden_orientation = fields.Selection([('north', 'North'), ('south', 'Sout'), ('east', 'East'), ('west', 'West')])
