8872606a522f3278b228bf551cd2bf81727646d2
DataMind <rune@battlekart.com>
2025-09-16 13:15:49 +0200

    Development commit

--- custom_estate/__manifest__.py
+++ custom_estate/__manifest__.py
@@ -6,7 +6,8 @@
     'depends': ['base'],  # Define dependencies
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
     ],
     'installable': True,
     'license': 'LGPL-3',
