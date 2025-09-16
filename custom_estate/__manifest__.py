e32bb8b88efea3778b130c85d23f92ed41c6c094
DataMind <rune@battlekart.com>
2025-09-16 11:22:23 +0200

    Development commit

--- custom_estate/__manifest__.py
+++ custom_estate/__manifest__.py
@@ -1,10 +1,10 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "18.0.1.0.10",  # Version
+    "version": "18.0.1.0.11",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
-
+        "security/ir.model.access.csv"
     ],
     "installable": True,
     'license': 'LGPL-3',
