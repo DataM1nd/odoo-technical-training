da5b9883672638362e7738edf21b6c5b19e25deb
DataMind <rune@battlekart.com>
2025-09-17 11:29:54 +0200

    Development commit

--- custom_estate/models/estate_property_offer.py
+++ custom_estate/models/estate_property_offer.py
@@ -1,5 +1,5 @@
 # Imports
-from odoo import fields, models
+from odoo import api, fields, models
 
 # Create the model
 class EstatePropertyOffer(models.Model):
@@ -12,3 +12,16 @@ class EstatePropertyOffer(models.Model):
     status = fields.Selection(selection=[('accepted', 'Accepted'), ('refused', 'Refused')], copy=False)
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property', required=True)
+    validity = fields.Integer('Validity (days)', default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
+
+    # Method for calculating the deadline when the validity field is changed
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.create_date or fields.Date.today(), days=record.validity)
+
+    # Method for calculating the validity when the date_deadline field is changed
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = record.date_deadline.day - record.create_date.day
