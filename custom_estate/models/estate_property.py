22d9e82bec13730fcc436a42cd30a3d8feea57ed
DataMind <rune@battlekart.com>
2025-09-17 09:31:40 +0200

    Development commit

--- custom_estate/models/estate_property.py
+++ custom_estate/models/estate_property.py
@@ -26,4 +26,4 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     salesperson_id = fields.Many2one('res.users', string='Salesman', default=lambda self: self.env.user)
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
-    tag_ids = fields.Many2many('estate.property.tag', string='tags')
+    tag_ids = fields.Many2many('estate.property.tag', string='Tags')
