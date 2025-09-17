036640db98e719186d426b690351472798e5f72d
DataMind <rune@battlekart.com>
2025-09-17 10:27:53 +0200

    Development commit

--- custom_estate/models/estate_property.py
+++ custom_estate/models/estate_property.py
@@ -1,5 +1,5 @@
 # Imports
-from odoo import fields, models
+from odoo import api, fields, models
 
 # Create the model
 class EstateProperty(models.Model):
@@ -9,16 +9,16 @@ class EstateProperty(models.Model):
 
     # Define fields for the model
     name = fields.Char('Title', required=True)
-    active = fields.Boolean(default=True, required=True)
     description = fields.Text()
+    active = fields.Boolean(default=True, required=True)
     postcode = fields.Char()
     date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3)) # We do "lambda self:" to convert it into a function (if we don't do this, we'll always get the same day)
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
+    facades = fields.Integer()
     bedrooms = fields.Integer(default=2)
     living_area = fields.Integer('Living Area (sqm)')
     garden_area = fields.Integer('Garden Area (sqm)')
-    facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
@@ -28,3 +28,9 @@ class EstateProperty(models.Model):
     partner_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+    total_area = fields.Integer('Total Area (sqm)', compute='_compute_total_area')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
